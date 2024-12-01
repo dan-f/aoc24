ab801e24ca3d00d05e1ce25f4b4273bac4b151bc
Daniel Friedman <dfriedman58@gmail.com>
2024-12-01 14:04:12 -0500

    Solved day 1 pt 1

--- src/d1.rs
+++ src/d1.rs
@@ -1,3 +1,7 @@
+use std::{io::BufRead, str::FromStr};
+
+use anyhow::Context;
+
 use crate::Day;
 
 pub struct D1;
@@ -8,20 +12,74 @@ impl Default for D1 {
     }
 }
 
-pub struct Solution;
+pub struct Input {
+    // observation! each num is 5 digits, meaning we can represent these lists
+    // with constant-space. (100,000 entries)
+    //
+    // we could almost represent them with a 32-bit number *except* for the fact
+    // that we could have multiple versions of the same number.
+    //
+    // So we could do something like a 100,000-long array where each element has
+    // an increment counter, and we iterate through them by:
+    // - when at an empty bucket, skip until we reach a full bucket
+    // - "count" a bucket by decrementing it
+    //
+    // This also means we don't have to do n * log(n) sorting
+    //
+    l: Vec<u32>,
+    r: Vec<u32>,
+}
+
+impl Input {
+    fn read(reader: impl BufRead) -> anyhow::Result<Self> {
+        let mut l: Vec<u32> = vec![];
+        let mut r: Vec<u32> = vec![];
+
+        for line in reader.lines() {
+            let line = line.context("Failed to read line")?;
+            let mut nums = line.split_whitespace();
+            let l_num = nums
+                .next()
+                .and_then(|x| u32::from_str(x).ok())
+                .context("Line in input not well-formatted (expected two numbers)")?;
+            let r_num = nums
+                .next()
+                .and_then(|x| u32::from_str(x).ok())
+                .context("Line in input not well-formatted (expected two numbers)")?;
+            l.push(l_num);
+            r.push(r_num);
+        }
+
+        l.sort();
+        r.sort();
+
+        Ok(Self { l, r })
+    }
+}
 
 impl Day for D1 {
-    type Solution = Solution;
+    type Input = Input;
+    type Outcome = u32;
 
     fn input_fname(&self) -> &'static str {
         "1"
     }
 
-    fn solve(&mut self, reader: &mut impl std::io::BufRead) -> Self::Solution {
-        todo!()
+    fn parse(&self, reader: impl BufRead) -> anyhow::Result<Self::Input> {
+        Input::read(reader)
     }
 
-    fn fmt(&self, soln: &Self::Solution) -> String {
-        todo!()
+    fn solve(&self, input: Self::Input) -> Self::Outcome {
+        let mut outcome = 0;
+
+        for (l, r) in input.l.iter().zip(input.r.iter()) {
+            outcome += l.abs_diff(*r)
+        }
+
+        outcome
+    }
+
+    fn fmt(&self, soln: &Self::Outcome) -> String {
+        soln.to_string()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,33 @@
 use std::{
     env::current_dir,
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{BufRead, BufReader},
 };
 
 pub mod d1;
 
+// TODO probably something like:
+// - refactor this whole type into something called "Solution" and rename `Solution` associated type to `Answer`
+// - then have `Day` have two solutions; one for pt1, one for pt2
+//
+// TODO refactor out anyhow::Error as this is "library" code
 pub trait Day: Default {
-    type Solution;
+    type Input;
+    type Outcome;
 
     /// The filename of the day's input relative to the local `input/` dir
     fn input_fname(&self) -> &'static str;
 
+    fn parse(&self, reader: impl BufRead) -> anyhow::Result<Self::Input>;
+
     /// Compute the solution given a buffered reader over the input file
-    fn solve(&mut self, reader: &mut impl BufRead) -> Self::Solution;
+    fn solve(&self, input: Self::Input) -> Self::Outcome;
 
     /// Format a solution for submission
-    fn fmt(&self, soln: &Self::Solution) -> String;
+    fn fmt(&self, soln: &Self::Outcome) -> String;
 }
 
-pub fn solve_day(mut day: impl Day) -> io::Result<String> {
+pub fn solve_day(day: impl Day) -> anyhow::Result<String> {
     let mut input_path = current_dir()?;
     input_path.push("input");
     input_path.push(day.input_fname());
@@ -27,6 +35,7 @@ pub fn solve_day(mut day: impl Day) -> io::Result<String> {
     let input_file = File::open(input_path)?;
     let mut reader = BufReader::new(input_file);
 
-    let soln = day.solve(&mut reader);
+    let input = day.parse(&mut reader)?;
+    let soln = day.solve(input);
     Ok(day.fmt(&soln))
 }
