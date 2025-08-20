b2b0bde58d510e165f56de2fc51e5105c1de41fd
Daniel Friedman <dfriedman58@gmail.com>
2025-08-20 14:04:53 -0400

    Day 8 pt 2

--- src/day/d8.rs
+++ src/day/d8.rs
@@ -7,14 +7,14 @@ pub struct D8;
 impl Day for D8 {
     type P1<'a> = P1;
 
-    type P2<'a> = P1;
+    type P2<'a> = P2;
 
     fn day() -> u8 {
         8
     }
 }
 
-pub struct P1 {}
+pub struct P1;
 
 impl<'a> Solution<'a> for P1 {
     type Input = Map;
@@ -25,12 +25,13 @@ impl<'a> Solution<'a> for P1 {
         let mut antinode_locs: HashSet<(isize, isize)> = HashSet::new();
         for (_, locs) in map.antennae_by_freq {
             for (i, (a1_col, a1_row)) in locs.iter().enumerate() {
+                let (a1_col, a1_row) = (*a1_col as isize, *a1_row as isize);
                 for j in i + 1..locs.len() {
-                    let (a2_col, a2_row) = locs[j];
-                    let d_col = (a2_col as isize) - (*a1_col as isize);
-                    let d_row = (a2_row as isize) - (*a1_row as isize);
-                    let antinode_1 = ((a2_col as isize) + d_col, (a2_row as isize) + d_row);
-                    let antinode_2 = ((*a1_col as isize) - d_col, (*a1_row as isize) - d_row);
+                    let (a2_col, a2_row) = (locs[j].0 as isize, locs[j].1 as isize);
+                    let d_col = a2_col - a1_col;
+                    let d_row = a2_row - a1_row;
+                    let antinode_1 = (a2_col + d_col, a2_row + d_row);
+                    let antinode_2 = (a1_col - d_col, a1_row - d_row);
                     if (0..map.cols as isize).contains(&antinode_1.0)
                         && (0..map.rows as isize).contains(&antinode_1.1)
                     {
@@ -40,6 +41,47 @@ impl<'a> Solution<'a> for P1 {
                         && (0..map.rows as isize).contains(&antinode_2.1)
                     {
                         antinode_locs.insert(antinode_2);
+                    }
+                }
+            }
+        }
+        Ok(antinode_locs.len())
+    }
+}
+
+pub struct P2;
+
+impl<'a> Solution<'a> for P2 {
+    type Input = Map;
+
+    type Output = usize;
+
+    fn solve(map: Self::Input) -> crate::harness::Result<Self::Output> {
+        let mut antinode_locs: HashSet<(isize, isize)> = HashSet::new();
+        for (_, locs) in map.antennae_by_freq {
+            for (i, (a1_col, a1_row)) in locs.iter().enumerate() {
+                let (a1_col, a1_row) = (*a1_col as isize, *a1_row as isize);
+                for j in i + 1..locs.len() {
+                    let (a2_col, a2_row) = (locs[j].0 as isize, locs[j].1 as isize);
+                    let d_col = a2_col - a1_col;
+                    let d_row = a2_row - a1_row;
+
+                    let mut antinode = (a1_col, a1_row);
+                    while (0..map.cols as isize).contains(&antinode.0)
+                        && (0..map.rows as isize).contains(&antinode.1)
+                    {
+                        antinode_locs.insert(antinode);
+                        antinode.0 -= d_col;
+                        antinode.1 -= d_row;
+                    }
+
+                    antinode = (a2_col, a2_row);
+                    while (0..map.cols as isize).contains(&antinode.0)
+                        && (0..map.rows as isize).contains(&antinode.1)
+                    {
+                        antinode_locs.insert(antinode);
+                        antinode.0 += d_col;
+                        antinode.1 += d_row;
                     }
                 }
             }
